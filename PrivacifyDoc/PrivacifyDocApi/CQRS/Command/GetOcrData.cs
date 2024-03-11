47428150e9e011c8e641a9eb919217dcde41ac4c
filip brodka <filip.brodka@gmail.com>
2024-03-11 20:37:22 +0100

    fix

---
+++ PrivacifyDoc/PrivacifyDocApi/CQRS/Command/GetOcrData.cs
@@ -0,0 +1,21 @@
+﻿namespace PrivacifyDoc.CQRS.Command
+{
+    public class OcrDataRetriever
+    {
+        private const string OcDataCollectionName = "OcrData";
+        
+        private readonly IMongoCollection<Option> _collection;
+        
+        // Constructor that initializes the MongoCollection required for operations
+        public OcrDataRetriever(IMongoDatabase database)
+        {
+            _collection = database.GetCollection<Option>(OcDataCollectionName);
+        }
+        
+        // Method to asynchronously retrieve a record with a specific 'id'
+        public async Task<Option> GetAsync(string id)
+        {
+            return await _collection.Find(new BsonDocument("_id", new ObjectId(id))).FirstOrDefaultAsync();
+        }
+    }
+}
--- PrivacifyDoc/PrivacifyDocApi/CQRS/Dispatcher/Dispatcher.cs
+++ PrivacifyDoc/PrivacifyDocApi/CQRS/Dispatcher/Dispatcher.cs
@@ -11,6 +11,27 @@ public class Dispatcher
 
     public TResult Query<TResult>(IQuery<TResult> query)
     {
-        // Locate the correct handler and invoke it
+        if (query == null)
+        {
+            throw new ArgumentNullException(nameof(query));
+        }
+        
+        var handlerType = typeof(IQueryHandler<,>).MakeGenericType(query.GetType(), typeof(TResult));
+
+        object handler = null; // Get handler object using dependency injection or service locator
+
+        if (handler == null)
+        {
+            throw new DispatcherException($"No handler registered for {nameof(query)}");
+        }
+
+        var handlerMethod = handlerType.GetMethod("Handle");
+
+        if (handlerMethod == null)
+        {
+            throw new DispatcherException($"No Handle method found for {nameof(query)}");
+        }
+
+        return (TResult) handlerMethod.Invoke(handler, new object[] {query});
     }
 }
